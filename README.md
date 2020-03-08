6811986e3ce6fa4a9100d682de757c6aa6d75477
tungnguyen741 <tung.nguyen21098@gmail.com>
2020-03-08 13:31:38 +0700

    'component-menuandBg-animate'

---
+++ README.md
@@ -0,0 +1,6 @@
+https://demo.kallyas.net/sneakers-shop/
+https://undsgn.com/uncode/homepages/portfolio-classic/
+https://undsgn.com/uncode/homepages/classic-agency/
+https://preview.themeforest.net/item/polo-responsive-multipurpose-html5-template/full_screen_preview/13708923?_ga=2.123544331.1064225064.1583587000-1069471049.1583570834
+http://www.themezaa.com/html/pofo/home-classic-web-agency.html
+http://preview.themeforest.net/item/pofo-creative-agency-corporate-and-portfolio-multipurpose-template/full_screen_preview/20645944?_ga=2.125513354.1629248223.1583587251-1069471049.1583570834
---
+++ script.js
@@ -0,0 +1,68 @@
+$(function(){
+	
+	$(window).scroll(function(){
+		var viTri = $(document).scrollTop();
+		 if(viTri > 200){
+		 	$('.navbar').addClass('menu_2');
+		 }
+		 else if(viTri < 200)
+		 {
+		 	$('.navbar').removeClass('menu_2');
+		 }
+		 	
+	});
+ 
+	$('.img_1').animate({
+ 		top:'-90px',
+ 		left: '-260px', 
+	},100);
+ 	 $('.img_1').css('transform','rotate('+-7+'deg)'); 
+
+	$('.img_2').animate({
+ 		top:'-100px',
+ 		right: '-25px', 
+	},100);
+ 	 $('.img_2').css('transform','rotate('+375+'deg)');
+ 	 $('.img_2').css('transform-origin','bottom');
+
+	$('.img_3').animate({
+	 		top:'350px',
+	 		left: '-400px', 
+		},100);
+
+	
+	$('.img_4').animate({
+	 		top:'500px',
+	 		left:"100px"
+		},600 );
+	$('.img_4').css('transform','rotate('+-10+'deg)');
+
+
+	$('.img_5').animate({
+	 		top:'585px',
+		},1700);
+	
+	$('.img_6').animate({
+			bottom:"360px",
+	 		left:'350px'
+		},100 );
+	$('.img_6').css('transform','rotate('+-51+'deg)');
+
+	$('.img_7').animate({
+			bottom:"370px",
+	 		left:'455px'
+		},100 );
+	$('.img_7').css('transform','rotate('+35+'deg)');
+
+	$('.img_8').animate({
+			bottom:"150px",
+	 		right:'100px'
+		},100 );
+	$('.img_8').css('transform','rotate('+15+'deg)');
+
+	$('.img_9').animate({
+			bottom:"0",
+	 		right:'-490px'
+		},100 );
+	$('.img_9').css('transform','rotate('+0+'deg)');
+});
